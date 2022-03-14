fa92fe3e14086521539458f9b8e8ce05285b1348
Szymon Tracz <szymontracz1@gmail.com>
2022-03-14 11:55:52 +0100

    UI - display_inventory function

--- ui.py
+++ ui.py
@@ -6,3 +6,14 @@ def display_board(board):
     Nothing
     '''
     pass
+
+
+def display_inventory(inventory: dict):
+    print('-----------------')
+    print('item name | count')
+    print('-----------------')
+    arr = list(inventory.items())
+    arr.sort(key =lambda x: x[1])
+    for line in arr:
+        print('{:>9} |{:>6}'.format(*line))
+    print('-----------------')
