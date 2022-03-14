1ba55d69ffa32baaee0e2713600dd7c9896d0a56
Wojciech Prusaczyk <wojciech.prusaczyk1@gmail.com>
2022-03-14 13:29:04 +0100

    display board

--- ui.py
+++ ui.py
@@ -1,11 +1,11 @@
 def display_board(board):
-    '''
-    Displays complete game board on the screen
+    for i in range(len(board)):
+        for j in range(len((board)[i])):
+            if j == (len((board)[i])-1):
+                print(str(board[i][j]), end="\n")
+            else:
+                print(str(board[i][j]) + " ", end="")
 
-    Returns:
-    Nothing
-    '''
-    pass
 
 
 def display_inventory(inventory: dict):
