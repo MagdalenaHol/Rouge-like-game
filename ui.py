de9844502a7167189a33e0ca475cd9f21c97503f
Szymon Tracz <szymontracz1@gmail.com>
2022-03-15 14:24:34 +0100

    dwa

--- ui.py
+++ ui.py
@@ -11,9 +11,7 @@ def display_board(board):
 def display_inventory(inventory: dict):
     print('-----------------')
     print('item name | count')
-    print('-----------------')
-    arr = list(inventory.items())
-    arr.sort(key =lambda x: x[1])
-    for line in arr:
-        print('{:>9} |{:>6}'.format(*line))
-    print('-----------------')
+fsewsfsefse
+fse
+fse
+fsewsfsefsese
