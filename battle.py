8377b4fd59e0eeb9fb5af6dd35a9587531446ef3
Szymon Tracz <szymontracz1@gmail.com>
2022-03-16 21:34:56 +0100

    battle - the last one working

--- battle.py
+++ battle.py
@@ -3,25 +3,23 @@ import util
 import main
 def new_battle(player, enemy, board):
     util.clear_screen()
-    print("Battle!")
-    attack = input('Do you want to attack? (y/n): ').lower()
-    if attack.startswith('y'):
-        while True:
-            enemy['health'] = enemy['health'] - player['damage']
-            if enemy['health'] < 0:
-                print('You killed a ', enemy['name'])
-                print(enemy['health'])
-                input('Press Enter')
-                break
-            else:
-                player['health'] = player['health'] - enemy['damage']
-                print(player['health'])
-                input('Press Enter')
-                if player['health'] < 0:
-                    again = input('You died! Wnna play again? (y/n): ')
-
-                    if again.startswith('y'):
-                        main.main()
-    
-    if attack.startswith('n'):
-        enemy = engine.put_enemy_on_board(board)
+    print("Battle!\nYou are fighting againt:", enemy['name'], enemy['health'], 'HP')
+    print("Your hit takes:", player['damage'], 'HP')
+    while True:
+        enemy['health'] = enemy['health'] - player['damage']
+        if enemy['health'] < 0:
+            print('You killed a ', enemy['name'])
+            input('Press Enter')
+            break
+        else:
+            print(enemy['name'], 'has ',enemy['health'], 'HP left')
+            player['health'] = player['health'] - enemy['damage']
+            if player['health'] <= 0:
+                again = input('You died! Wanna play again? (y/n): ')
+                if again.startswith('y'):
+                    main.main()
+                else:
+                    exit()
+            print('You have ',player['health'], 'HP left')
+            input('Press Enter')
+        
